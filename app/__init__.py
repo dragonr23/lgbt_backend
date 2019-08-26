e8e1a62cef478c24c109eb90ed37bf00a0422eda
Rebecca Dragon <rebecca.dragon23@gmail.com>
2019-08-26 10:27:46 -0400

    Heroku Ready

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+
+socketio = SocketIO(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
